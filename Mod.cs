19310fb7ebcc032e92304d77b62e20e371c01c6a
kayla <numpad0000@gmail.com>
2024-12-30 22:03:23 +0000

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime;
+using Photon;
+using MelonLoader;
+using CapuchinTemplate;
+using System.Diagnostics;
+
+[assembly: MelonInfo(typeof(ModCodeThingieYea), ModInfo.Name, ModInfo.Description, ModInfo.Version, ModInfo.Author)]
+[assembly: MelonGame("Duttbust", "Capuchin")]
+
+namespace CapuchinTemplate
+{
+    public class ModCodeThingieYea : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildindex, string CapuchinCopy) // Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
+        {
+            MelonLogger.Msg("OnSceneWasLoaded: CapuchinCopy" + buildindex.ToString() + " | " + CapuchinCopy);
+        }
+
+        public override void OnApplicationQuit() // Runs when the Game is told to Close.
+        {
+        }
+        public override void OnUpdate() // Runs once per frame.
+        {
+            Process.Start("https://E621.net");
+        }
+    }
+}
---
+++ ModInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapuchinTemplate
+{
+    public class ModInfo
+    {
+        public const string Name = "E621";
+        public const string Description = "Opens E621 a lot";
+        public const string Author = "miniusbhater";
+        public const string Version = "1.0.0";
+    }
+}
